0269130c89f4220b4574c24be4ecda9d3db5317c
Utkan <utkancelik@hacettepe.edu.tr>
2022-12-08 18:35:48 +0300

    Add project files.

---
+++ More About Classes and Methods/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace More_About_Classes_and_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            Car.MyMethod();
+
+            /*
+            myCar.Make = "Oldmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+            */
+
+
+            /*
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
+
+            Car myOtherCar;
+            myOtherCar = myCar;
+
+            Console.WriteLine($"{myOtherCar.Make}, {myOtherCar.Model}," +
+                $" {myOtherCar.Year}, {myOtherCar.Color}");
+
+            myOtherCar.Model = "98";
+
+            Console.WriteLine($"{myCar.Make}, {myCar.Model}," +
+                $" {myCar.Year}, {myCar.Color}");
+
+            myOtherCar = null;
+
+            //Console.WriteLine($"{myOtherCar.Make}, {myOtherCar.Model}," +
+            //    $" {myOtherCar.Year}, {myOtherCar.Color}");
+
+            myCar = null;
+            */
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        /*
+        public Car()
+        {
+            // You could load from a configuration file,
+            // a database, etc.
+            Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        */
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Called the static MyMethod.");
+            //Console.WriteLine(Make);
+        }
+    }
+}
---
+++ Understanding Classes/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Understanding_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlas Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year} {myCar.Color}");
+
+            Console.WriteLine("{0}", myCar.DetermineTheMarketValue());
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+        }
+
+        //private static decimal DetermineMarketValue(Car car)
+        //{
+        //    decimal carValue = 100.0M;
+
+        //    return carValue;
+        //}
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+
+        public decimal DetermineTheMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+            {
+                carValue = 10000;
+            }
+            else
+            {
+                carValue = 2000;
+            }
+
+
+            return carValue;
+        }
+        //public Car(string _Make, string _Model, int _Year, string _Color) 
+        //{
+        //    Make = _Make;
+        //    Model = _Model;
+        //    Year = _Year;
+        //    Color = _Color;
+        //}
+    }
+}
+
